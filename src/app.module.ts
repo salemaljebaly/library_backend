f0213e66fdfcf70e3f85db326678702107613691
salemaljebaly <salemaljebaly@gmail.com>
2022-05-06 16:06:20 +0200

    config typeorm , create database and test connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],

81855256f1e77001883fff2127de45fd03ded2d0
salemaljebaly <salemaljebaly@gmail.com>
2022-05-31 14:33:05 +0200

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: 'http://158.101.167.198:3000',
     allowedHeaders:'*'
   });
   const config = new DocumentBuilder()
